dca150d4fca1c4914210dc205c6bea7f3225edad
yishnu96 <yishnupramanik96@gmail.com>
2020-07-04 20:41:20 +0530

    done with images

---
+++ app/src/main/java/com/danielkim/yas_manager/fragments/SettingsFragment.java
@@ -0,0 +1,41 @@
+package com.danielkim.yas_manager.fragments;
+
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+import com.danielkim.yas_manager.BuildConfig;
+import com.danielkim.yas_manager.MySharedPreferences;
+import com.danielkim.yas_manager.R;
+import com.danielkim.yas_manager.activities.SettingsActivity;
+
+public class SettingsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+
+        CheckBoxPreference highQualityPref = (CheckBoxPreference) findPreference(getResources().getString(R.string.pref_high_quality_key));
+        highQualityPref.setChecked(MySharedPreferences.getPrefHighQuality(getActivity()));
+        highQualityPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                MySharedPreferences.setPrefHighQuality(getActivity(), (boolean) newValue);
+                return true;
+            }
+        });
+
+        Preference aboutPref = findPreference(getString(R.string.pref_about_key));
+        aboutPref.setSummary(getString(R.string.pref_about_desc, BuildConfig.VERSION_NAME));
+        aboutPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                LicensesFragment licensesFragment = new LicensesFragment();
+                licensesFragment.show(((SettingsActivity)getActivity()).getSupportFragmentManager().beginTransaction(), "dialog_licenses");
+                return true;
+            }
+        });
+    }
+}
